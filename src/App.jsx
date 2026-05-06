f10a0104441b69ebd4b66867ade9d9133fc1e58a
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-05-05 21:07:11 -0300

    feat: atualiza rotas com Sidebar e novas páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
+import AnaliseGastos from "./pages/AnaliseGastos";
+import Transacoes from "./pages/Transacoes";
+import InsightsPage from "./pages/InsightsPage";
+import "./styles/global.css";
 
 function App() {
-  return <Home />;
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <div style={{ display: "flex" }}>
+          <Sidebar />
+          <main style={{ flex: 1, overflow: "auto" }}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Home />} />
+              <Route path="/analise" element={<AnaliseGastos />} />
+              <Route path="/transacoes" element={<Transacoes />} />
+              <Route path="/insights" element={<InsightsPage />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
